8aafb6e47298c445b1114d30aa056d84e5e89ce4
Francisco Beltran <paco.beltran.contractor@macmillan.com>
2024-05-27 09:21:14 -0600

    Added test case with first and third transformer

--- src/test/java/com/transformers/demo/controller/TransformerRestControllerTest.java
+++ src/test/java/com/transformers/demo/controller/TransformerRestControllerTest.java
@@ -64,4 +64,42 @@ class TransformerRestControllerTest {
 		assertTrue(response.contains("\"transformedValue\":\"This is just some text\""));
         assertTrue(response.contains(originalText));
 	}
+
+    @Test
+	void complexTransformShouldTransliterate() throws Exception {
+        String originalText = "This is just Βρίσκεστε στην αρχική σελίδα του: Δημόσιου!";
+
+        JSONArray JSONParameters1 = new JSONArray();
+        JSONParameters1.put("!");
+
+        JSONObject JSONTransformer = new JSONObject();
+        JSONTransformer.put("group", "group1");
+        JSONTransformer.put("transformerId", "t1");
+        JSONTransformer.put("parameters",JSONParameters1);
+
+        JSONArray JSONParameters3 = new JSONArray();
+        JSONObject JSONTransformer3 = new JSONObject();
+        JSONTransformer3.put("group", "group1");
+        JSONTransformer3.put("transformerId", "t3");
+        JSONTransformer3.put("parameters",JSONParameters3);
+
+        JSONArray JSONTransformersArray = new JSONArray();
+        JSONTransformersArray.put(JSONTransformer);
+        JSONTransformersArray.put(JSONTransformer3);
+
+        JSONObject JSONElements = new JSONObject();
+        JSONElements.put("value", originalText);
+        JSONElements.put("transformers", JSONTransformersArray);
+
+        JSONArray JSONElementsArray = new JSONArray();
+        JSONElementsArray.put(JSONElements);
+
+        JSONObject JSONBaseObject = new JSONObject();
+        JSONBaseObject.put("elements", JSONElementsArray);
+
+        HttpEntity<String> request = new HttpEntity<String>(JSONBaseObject.toString(), headers);
+        String response = restTemplate.postForObject(String.format(requestUrl, port),request,String.class);
+		assertTrue(response.contains("\"transformedValue\":\"This is just Vriskeste stin archiki selida tou: Dimosiou\""));
+        assertTrue(response.contains(originalText));
+	}
 }
